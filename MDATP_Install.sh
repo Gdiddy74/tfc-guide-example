799e217fe76826957fe911ae03f96f9413f8df54
Gdiddy74 <59712084+Gdiddy74@users.noreply.github.com>
2020-11-30 12:33:10 +0000

    Update MDATP_Install.sh

--- MDATP_Install.sh
+++ MDATP_Install.sh
@@ -1,5 +1,5 @@
-sudo yum install yum-utils &&
+sudo yum -y install yum-utils &&
 sudo yum-config-manager --add-repo=https://packages.microsoft.com/config/rhel/8/prod.repo &&
 sudo rpm --import http://packages.microsoft.com/keys/microsoft.asc &&
-yum makecache &&
-sudo yum install mdatp
+sudo yum makecache &&
+sudo yum -y install mdatp
